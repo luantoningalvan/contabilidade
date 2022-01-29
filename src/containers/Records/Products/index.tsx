c25610cee7b098f271ca5261d60db575dc839a9f
Luan Tonin Galvan <ltoningalvan@gmail.com>
2022-01-29 09:01:39 -0300

    feat: cria central de cadastros

---
+++ src/containers/Records/Products/index.tsx
@@ -0,0 +1,96 @@
+import { useCallback, useEffect, useState } from "react";
+import { api } from "../../../services/api";
+import {
+  Box,
+  Heading,
+  Button,
+  SimpleGrid,
+  Input,
+  Text,
+  InputLeftElement,
+  InputGroup,
+  Image,
+} from "@chakra-ui/react";
+import { FiPlus, FiSearch } from "react-icons/fi";
+import { NewProduct } from "../../../shared/NewProduct";
+type Product = {
+  id: number;
+  name: string;
+  thumb: string;
+};
+
+export function Products() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [newProductDialog, setNewProductDialog] = useState(false);
+
+  const fetchProducts = useCallback(() => {
+    api.get("products").then((res) => {
+      setProducts(res.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
+
+  return (
+    <>
+      {newProductDialog && (
+        <NewProduct
+          open={newProductDialog}
+          onClose={() => setNewProductDialog(false)}
+          afterSubmit={fetchProducts}
+        />
+      )}
+      <div>
+        <Box display="flex" justifyContent="space-between">
+          <InputGroup>
+            <InputLeftElement>
+              <FiSearch />
+            </InputLeftElement>
+            <Input placeholder="Buscar produto" width="auto" />
+          </InputGroup>
+          <Button
+            colorScheme="purple"
+            onClick={() => setNewProductDialog(true)}
+            leftIcon={<FiPlus />}
+          >
+            Novo produto
+          </Button>
+        </Box>
+
+        <SimpleGrid columns={5} spacing={4} mt={4}>
+          {products.map((product) => (
+            <Box
+              key={product.id}
+              display="flex"
+              borderWidth={1}
+              rounded={6}
+              cursor="pointer"
+            >
+              <Image
+                roundedBottomLeft={4}
+                roundedTopLeft={6}
+                src={product.thumb}
+                alt={product.name}
+                h="145px"
+              />
+              <Box
+                p={4}
+                display="flex"
+                justifyContent="center"
+                flexDirection="column"
+                bg="gray.50"
+              >
+                <Heading size="sm" noOfLines={3} mb={2}>
+                  {product.name}
+                </Heading>
+                <Text color="gray.600">03 unidades</Text>
+              </Box>
+            </Box>
+          ))}
+        </SimpleGrid>
+      </div>
+    </>
+  );
+}
--- src/containers/Records/index.tsx
+++ src/containers/Records/index.tsx
@@ -1,5 +1,42 @@
 import { Layout } from "../../components/Layout";
+import {
+  Tabs,
+  TabList,
+  Tab,
+  TabPanel,
+  TabPanels,
+  Box,
+  Heading,
+} from "@chakra-ui/react";
+import { Products } from "./Products";
 
 export function Records() {
-  return <Layout>Cadastros</Layout>;
+  return (
+    <Layout>
+      <Tabs variant="soft-rounded" colorScheme="purple">
+        <Box
+          display="flex"
+          alignItems="center"
+          gap={4}
+          h="56px"
+          p={4}
+          borderBottomWidth={1}
+        >
+          <Heading size="md">Central de cadastros</Heading>
+          <TabList>
+            <Tab>Produtos</Tab>
+            <Tab>Clientes</Tab>
+          </TabList>
+        </Box>
+        <TabPanels>
+          <TabPanel>
+            <Products />
+          </TabPanel>
+          <TabPanel>
+            <p>two!</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </Layout>
+  );
 }
