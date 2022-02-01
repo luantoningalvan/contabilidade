bbd669da7f3bbcc915e3931c42e923b3a5f3200d
Luan Tonin Galvan <ltoningalvan@gmail.com>
2022-02-01 18:48:25 -0300

    fix: evita valores vazios na função parseOptions

--- web/src/utils/parseOptions.ts
+++ web/src/utils/parseOptions.ts
@@ -2,7 +2,11 @@ export const parseOptions = (options: any) => {
   let params: string[] = [];
 
   Object.entries(options).map(([key, value]) => {
-    if (["string", "number"].includes(typeof value) && value !== "") {
+    if (
+      !!value &&
+      ["string", "number"].includes(typeof value) &&
+      value !== ""
+    ) {
       params.push(`${key}=${value}`);
     }
   });
